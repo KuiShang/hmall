b3a60d0cef0b34c9dbf1dbe037f225d66bc07589
GRANDSOFT\shangkk <shangkk@glodon.com>
2024-07-12 14:38:58 +0800

    add

--- trade-service/src/main/java/com/hmall/trade/TradeApplication.java
+++ trade-service/src/main/java/com/hmall/trade/TradeApplication.java
@@ -1,17 +1,16 @@
-package com.hmall.cart;
+package com.hmall.trade;
 
-import com.hmall.api.client.ItemClient;
 import com.hmall.api.config.DefaultFeignConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@MapperScan("com.hmall.cart.mapper")
+@EnableFeignClients(basePackages = "com.hmall.api.client", defaultConfiguration = DefaultFeignConfig.class)
+@MapperScan("com.hmall.trade.mapper")
 @SpringBootApplication
-@EnableFeignClients(defaultConfiguration = DefaultFeignConfig.class, clients = {ItemClient.class})
-public class CartApplication {
+public class TradeApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CartApplication.class, args);
+        SpringApplication.run(TradeApplication.class, args);
     }
 }
